9d62499786c9ebad9e48eb22e77af005c166a80c
Ross Mechanic <ross.mechanic@icloud.com>
2019-04-22 15:46:45 -0400

    Revert "Try updating tests"
    This reverts commit 6e065f3163998e008c0592be108c82af727b8517.

--- simple_history/tests/tests/test_models.py
+++ simple_history/tests/tests/test_models.py
@@ -1315,9 +1315,9 @@ class MultiDBWithUsingTest(TestCase):
     keyword argument in `save()`.
     """
 
-    databases = {"default", "other"}
-
-    if django.VERSION < (2, 2, 0, "final"):
+    if django.VERSION >= (2, 2, 0, "final"):
+        databases = {"default", "other"}
+    else:
         multi_db = True
 
     db_name = "other"
@@ -1439,7 +1439,8 @@ class ForeignKeyToSelfTest(TestCase):
 
 @override_settings(**database_router_override_settings)
 class MultiDBExplicitHistoryUserIDTest(TestCase):
-    databases = {"default", "other"}
+    if django.VERSION >= (2, 2):
+        databases = {"default", "other"}
 
     def setUp(self):
         self.user = get_user_model().objects.create(
@@ -1570,9 +1571,9 @@ class RelatedNameTest(TestCase):
 
 @override_settings(**database_router_override_settings_history_in_diff_db)
 class SaveHistoryInSeparateDatabaseTestCase(TestCase):
-    databases = {"default", "other"}
-
-    if django.VERSION < (2, 2, 0, "final"):
+    if django.VERSION >= (2, 2, 0, "final"):
+        databases = {"default", "other"}
+    else:
         multi_db = True
 
     def setUp(self):
